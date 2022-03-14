27eb56189ee75c683f9596a3dc6abcbdffbe1add
404th <umarov.doniyor.2002@gmail.com>
2022-03-14 20:52:34 +0500

    Some changes

--- internal/adapters/framework/left/grpc/server.go
+++ internal/adapters/framework/left/grpc/server.go
@@ -1,1 +1,31 @@
 package grpc
+
+import (
+	"net"
+
+	"github.com/404th/portfolio_app/internal/adapters/framework/right/db"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	db *db.DB
+}
+
+func NewServer(db *db.DB) *Server {
+	return &Server{db}
+}
+
+func (s *Server) Run(port string) error {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		return err
+	}
+
+	grpcServer := grpc.NewServer()
+
+	if err = grpcServer.Serve(lis); err != nil {
+		return err
+	}
+
+	return nil
+}
